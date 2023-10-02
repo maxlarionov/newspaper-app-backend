ed4faa3ac188e15f5da22768a3fc5f4bd5e3d19d
maxlarionov <maksym.larionov@gmail.com>
2023-10-02 20:16:46 +0300

    v0.701 - upload image by multer

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
+const multer = require('multer')
 
 require('dotenv').config()
 
@@ -11,6 +12,17 @@ const articlesRouter = require('./routes/articles')
 const tagsRouter = require('./routes/tags')
 
 const app = express()
+
+const storage = multer.diskStorage({
+	destination: (_, __, cb) => {
+		cb(null, 'uploads')
+	},
+	filename: (_, file, cb) => {
+		cb(null, file.originalname)
+	}
+})
+
+const upload = multer({ storage })
 
 app.use(logger('dev'))
 app.use(cors())
@@ -21,5 +33,11 @@ app.use(cookieParser())
 app.use('/api/users', usersRouter)
 app.use('/api/articles', articlesRouter)
 app.use('/api/tags', tagsRouter)
+app.use('/api/upload', upload.single('image'), (req, res) => {
+	res.json({
+		url: `/api/uploads/${req.file.originalname}`
+	})
+})
+app.use('/api/uploads', express.static('uploads'))
 
 module.exports = app
